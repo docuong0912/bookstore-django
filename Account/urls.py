e4213dbdd4447800293e187658e5f33ed15ebb7c
docuong0912 <ndcuong912@gmail.com>
2026-05-03 15:01:47 +0700

    fix bug

--- Account/urls.py
+++ Account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     # API Đăng ký tài khoản mới
     path('register/', views.register, name='register'),
     
     # API Đăng nhập
-    path('login/', views.user_login, name='login'),
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='login'),
 ]
